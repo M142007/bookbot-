3559ba41045dc85c7efa54149a598ea28a5d5c53
jey <i01cutie1012@gmail.com>
2025-11-24 21:00:10 -0500

    read text

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print ("greetings boots")
+def get_book_text(frankenstein):
+    with open(frankenstein) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+    
+    
+
+main()
+
